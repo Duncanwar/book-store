b4e3f8a0e5c8b0f4db6b51f1417e546e7b422b2d
Duncanwar <semunda9@gmail.com>
2023-10-11 00:40:57 +0200

    added

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {PORT} from './config.js'
+import booksRoute from './routes/booksRoute.js'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/books", booksRoute)
 app.get("/",(req,res)=>{
     return res.status(234).send("Welcome to Express")
 })
