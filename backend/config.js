851f1b0450ab0a4c6bb317a4330202398e2eeabd
Duncanwar <semunda9@gmail.com>
2023-10-11 18:04:08 +0200

    approved

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000
+
+export const mongoDBURL = "mongodb+srv://semunda9:kibuye08@cluster0.smiafgs.mongodb.net/?retryWrites=true&w=majority"
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
-import {PORT} from './config.js'
+import cors from 'cors'
+import mongoose  from 'mongoose';
+import {PORT, mongoDBURL} from './config.js'
 import booksRoute from './routes/booksRoute.js'
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 app.use("/books", booksRoute)
 app.get("/",(req,res)=>{
     return res.status(234).send("Welcome to Express")
 })
 
-app.listen(PORT,()=>{
-    console.log(`App is listening on ${PORT}`)
-}); 
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('App connected to database')
+    app.listen(PORT,()=>{
+        console.log(`App is listening on ${PORT}`)
+    }); 
+})
+.catch((error)=>{
+    console.log(error)
+})
+
