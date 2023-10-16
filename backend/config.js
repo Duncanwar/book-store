7bab151526a128183c990deeb5680f8eaa8b817f
Duncanwar <semunda9@gmail.com>
2023-10-16 23:27:30 +0200

    me yo github

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
 export const PORT = 5000
 
-export const mongoDBURL = "mongodb+srv://semunda9:kibuye08@cluster0.smiafgs.mongodb.net/?retryWrites=true&w=majority"
+export const mongoDBURL = "mongodb://127.0.0.1:27017/bookstore"
+
+// "mongodb+srv://semunda9:kibuye08@cluster0.smiafgs.mongodb.net/?retryWrites=true&w=majority"
--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,40 @@
 import express from 'express';
-import cors from 'cors'
-import mongoose  from 'mongoose';
-import {PORT, mongoDBURL} from './config.js'
-import booksRoute from './routes/booksRoute.js'
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
-app.use("/books", booksRoute)
-app.get("/",(req,res)=>{
-    return res.status(234).send("Welcome to Express")
-})
+app.get('/', (request, response) => {
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
 
-mongoose.connect(mongoDBURL).then(()=>{
-    console.log('App connected to database')
-    app.listen(PORT,()=>{
-        console.log(`App is listening on ${PORT}`)
-    }); 
-})
-.catch((error)=>{
-    console.log(error)
-})
+app.use('/books', booksRoute);
 
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
